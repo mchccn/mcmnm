2a496322fdae92980d77347ddab8963134cbdfb1
kelsny <cursors.owo.uwu@gmail.com>
2023-02-17 23:55:35 -0600

    created parts lists

---
+++ src/data/skinParts.ts
@@ -0,0 +1,19 @@
+export const partsPriorities = ([
+    [
+        "boy-black-hair", // hair goes over head
+        "girl-black-hair", // hair goes over head
+    ],
+
+    ["white-shoes"], // shoes should go over pants
+
+    ["cargo-pants"], // needs to cover waist
+
+    ["exposed-shoulder-strap"], // must be drawn on over arms
+    ["black-sweater"],
+
+    ["base-skin"], // base
+] as const).flat();
+
+export type PartName = typeof partsPriorities[number];
+
+export const partsList = [...partsPriorities].sort() as PartName[];
